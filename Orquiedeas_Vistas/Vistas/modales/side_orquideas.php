c06e2ec92f5371d5262882cc69a810581247cf78
Alberto <scontrerasa3@miumg.edu.gt>
2024-10-01 16:01:56 -0600

    arreglos xd

--- Orquiedeas_Vistas/Vistas/modales/side_orquideas.php
+++ Orquiedeas_Vistas/Vistas/modales/side_orquideas.php
@@ -2,7 +2,7 @@
     <button class="toggle-button" id="toggle-button">☰</button>
     <h2>Admin Panel</h2>
     <ul>
-        <li><a href="dashboard.php" data-target="dashboard.php"><i class="fas fa-home"></i> <span>Inicio</span></a></li>
+        <li><a href="dashboard.php" data-target="#"><i class="fas fa-home"></i> <span>Inicio</span></a></li>
         <li><a href="Neva_orquidea.php" data-target="Neva_orquidea.php"><i class="fas fa-seedling"></i> <span>Registro de Orquídeas</span></a></li>
         <li><a href="#" data-target="../Vistas/Cruds/Crud_Orquideas.php"><i class="fas fa-users"></i> <span>Ver Orquídeas</span></a></li>
         <li><a href="#" data-target="logout.php"><i class="fas fa-sign-out-alt"></i> <span>Cerrar Sesión</span></a></li>
