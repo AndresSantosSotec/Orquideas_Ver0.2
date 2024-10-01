c769f1f3eb2347b599de1fb3038b468038ec1c52
Pablo Santos <121809720+AndresSantosSotec@users.noreply.github.com>
2024-10-01 15:56:11 -0600

    Cambios xd

--- Orquiedeas_Vistas/Vistas/modales/side_usu.php
+++ Orquiedeas_Vistas/Vistas/modales/side_usu.php
@@ -1,6 +1,7 @@
 <div class="sidebar" id="sidebar">
     <button class="toggle-button" id="toggle-button">☰</button>
     <h2>Admin Panel</h2>
+    
     <ul>
         <li><a href="dashboard.php" data-target="dashboard.php"><i class="fas fa-home"></i> <span>Inicio</span></a></li>
         <li><a href="#"><i class="fas fa-seedling"></i> <span>Registro de Participantes</span></a></li>
