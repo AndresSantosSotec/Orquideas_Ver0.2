dbbc09df1e592bbd2256d575d5968e65370e56c4
Alberto <scontrerasa3@miumg.edu.gt>
2024-10-08 16:02:40 -0600

    cambios alberto

--- Orquiedeas_Vistas/Vistas/modales/side_juzga.php
+++ Orquiedeas_Vistas/Vistas/modales/side_juzga.php
@@ -4,6 +4,6 @@
     <ul>
         <li><a href="dashboard.php" data-target="dashboard.php"><i class="fas fa-home"></i> <span>Inicio</span></a></li>
         <li><a href="#" data-target="../Vistas/Cards/cards_juzgamiento/Add_ganador.php"><i class="fas fa-award"></i> <span>Designar Ganadores</span></a></li>
-        <li><a href="#" data-target="../Vistas/cards_juzgamiento/list_ganadores.php"><i class="fas fa-medal"></i> <span>Listado de Ganadores</span></a></li>
+        <li><a href="#" data-target="../Vistas/Cards/cards_juzgamiento/list_ganadores.php"><i class="fas fa-medal"></i> <span>Listado de Ganadores</span></a></li>
     </ul>
 </div>
